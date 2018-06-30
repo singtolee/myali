7126b8708d5b1ca948a8c5e8ffb510cc564a34aa
Kannaphat  Suwannaphat <juang@Kannaphats-MacBook-Pro.local>
2018-07-01 01:22:41 +0700

    add to cart has solution

--- src/app/qtypicker/qtypicker.component.ts
+++ src/app/qtypicker/qtypicker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-qtypicker',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QtypickerComponent implements OnInit {
 
+  @Input() public size;
+  @Input() public color;
+  qty = 0
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  inc(){
+    this.qty = this.qty + 1
+
+  }
+
+  des(){
+    this.qty = this.qty -1
+    if(this.qty<0){
+      this.qty=0
+    }
+
   }
 
 }
