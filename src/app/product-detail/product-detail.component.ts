d4a4258f79f3e362b5070fb8f3c7267cc0160c9d
singtolee <liqiang0618@outlook.com>
2018-07-18 14:06:49 +0700

    go up and go back are implemented

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -4,6 +4,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { Product } from '../tools/Product';
 import { PassPrdObjectService } from '../pass-prd-object.service';
 import { take } from 'rxjs/operators'
+import { ViewportScroller, Location } from '@angular/common'
 
 @Component({
   selector: 'app-product-detail',
@@ -16,7 +17,7 @@ export class ProductDetailComponent implements OnInit {
   product:Product;
 
 
-  constructor(private route: ActivatedRoute,private data:PassPrdObjectService,private db:AngularFirestore) { }
+  constructor(private route: ActivatedRoute,private data:PassPrdObjectService,private db:AngularFirestore, private vps:ViewportScroller, private location: Location) { }
 
   ngOnInit() {
     this.data.currentProduct.subscribe(prd =>this.product = prd);
@@ -39,6 +40,13 @@ export class ProductDetailComponent implements OnInit {
 
   isObject(sth){
     return typeof sth === 'object';
+  }
+
+  goup(){
+    this.vps.scrollToPosition([0,0])
+  }
+  goback(){
+    this.location.back()
   }
 
 }
