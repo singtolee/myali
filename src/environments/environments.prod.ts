b5a943d97f42d4dcebf64364afda20a901bc2752
QIANG LI <qiangli@QIANGs-Mac-mini.local>
2018-08-01 21:52:06 +0700

    redo loading spiiner

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDRCiTgg0SNiRwUchDV70M8g6Kw2pTm88w",
+        authDomain: "alitoyou-168.firebaseapp.com",
+        databaseURL: "https://alitoyou-168.firebaseio.com",
+        projectId: "alitoyou-168",
+        storageBucket: "alitoyou-168.appspot.com",
+        messagingSenderId: "971853257033"
+    }
+
+};
