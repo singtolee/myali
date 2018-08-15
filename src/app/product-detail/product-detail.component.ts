57cf6645410ad762c936f1d9426430aad537346a
singtolee <liqiang0618@outlook.com>
2018-08-15 17:19:48 +0700

    renew banner

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -4,6 +4,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { Product } from '../tools/Product';
 import { PassPrdObjectService } from '../pass-prd-object.service';
 import { ViewportScroller, Location } from '@angular/common'
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-detail',
@@ -33,7 +34,7 @@ export class ProductDetailComponent implements OnInit {
       
       this.db.collection<Product>('PRODUCTS',ref=>{
         return ref.where("pid", "==", Number(this.id)) //this.id typeof is string, so convert to number
-      }).valueChanges().subscribe((data)=>{
+      }).valueChanges().pipe(take(1)).subscribe((data)=>{
         this.product = data[0];
         this.showSpinner = false;
       })
